7005c833a7f21b0e7117b37cd3819bd208d82453
laaouatni <laaouatni.anas@outlook.it>
2022-08-11 21:28:32 +0200

    getLine -> getCode()

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ class Gcommands extends GcodeAPI {
     this.lineOfCode = "⚠️ No line of code - please use getLine() method first";
   }
 
-  getLine() {
+  getCode() {
     this.setLastPosition();
     this.lineOfCode = `${this.prefix} X${this.x} Y${this.y} Z${this.z}`;
     return this.lineOfCode;
@@ -23,6 +23,7 @@ class Gcommands extends GcodeAPI {
 class G0 extends Gcommands {
   constructor(_xyzObj) {
     super();
+    // these line are repetitive
     this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
     this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
     this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
@@ -34,6 +35,7 @@ class G0 extends Gcommands {
 class G1 extends Gcommands {
   constructor(_xyzObj) {
     super();
+    // these line are repetitive
     this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
     this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
     this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
@@ -45,6 +47,7 @@ class G1 extends Gcommands {
 class G2 extends Gcommands { 
   constructor(_xyzObj) {
     super();
+    // these line are repetitive
     this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
     this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
     this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
@@ -56,6 +59,7 @@ class G2 extends Gcommands {
 class G3 extends Gcommands {
   constructor(_xyzObj) {
     super();
+    // these line are repetitive
     this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
     this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
     this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
