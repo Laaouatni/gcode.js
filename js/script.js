a6de5018b92bb5472ff10dadcf0dd515d410bbc1
laaouatni <laaouatni.anas@outlook.it>
2022-08-11 21:08:35 +0200

    trying with 3 commands in classes

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 class GcodeAPI {
 }
 
-class Gline extends GcodeAPI {
+class Gcommands extends GcodeAPI {
   constructor() {
     super();
-    this.lineOfCode = undefined;
+    this.lineOfCode = "⚠️ No line of code - please use getLine() method first";
   }
 
   getLine() {
@@ -20,7 +20,7 @@ class Gline extends GcodeAPI {
   }
 }
 
-class G0 extends Gline {
+class G0 extends Gcommands {
   constructor(_xyzObj) {
     super();
     this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
@@ -31,7 +31,7 @@ class G0 extends Gline {
   }
 }
 
-class G1 extends Gline {
+class G1 extends Gcommands {
   constructor(_xyzObj) {
     super();
     this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
@@ -41,3 +41,27 @@ class G1 extends Gline {
     this.prefix = `G1`;
   }
 }
+
+class G2 extends Gcommands { 
+  constructor(_xyzObj) {
+    super();
+    this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
+    this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
+    this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
+
+    this.prefix = `G2`;
+  }
+}
+
+class G3 extends Gcommands {
+  constructor(_xyzObj) {
+    super();
+    this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
+    this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
+    this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
+
+    this.prefix = `G3`;
+  }
+}
+
+console.log(new G3({ x: 1, y: 2, z: 3 }).getLine());
