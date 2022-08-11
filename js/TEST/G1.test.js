6152bcd6365d52fff823aad48705a0526b56221e
laaouatni <laaouatni.anas@outlook.it>
2022-08-12 00:24:03 +0200

    better testing, code is splitted to more folders

---
+++ js/TEST/G1.test.js
@@ -0,0 +1,42 @@
+import G1 from "../../API/Gcommands/G/G1.js";
+
+export default function G1test() {
+  // 0
+  console.log(
+    new G1({
+      x: 1,
+      y: 2,
+    }).getCode(),
+  );
+
+  // 1
+  console.log(
+    new G1({
+      x: 3,
+      y: 4,
+    }).getCode(),
+  );
+
+  // 2
+  console.log(
+    new G1({
+      x: 5,
+    }).getCode(),
+  );
+
+  // 3
+  console.log(
+    new G1({
+      z: 6,
+    }).getCode(),
+  );
+
+  // 4
+  console.log(
+    new G1({
+      x: 7,
+      y: 8,
+      z: 9,
+    }).getCode(),
+  );
+}
--- js/test.js
+++ js/test.js
@@ -1,36 +1,5 @@
-import G1 from "../API/Gcommands/G/G1.js";
-import G0 from "../API/Gcommands/G/G0.js";
+import G0test from './TEST/G0.test.js';
+G0test();
 
-console.log(
-  new G1({
-    x: 1,
-    y: 2,
-  }).getCode(),
-);
-
-console.log(
-  new G1({
-    x: 3,
-    y: 4,
-  }).getCode(),
-);
-
-console.log(
-  new G1({
-    x: 5,
-  }).getCode(),
-);
-
-console.log(
-  new G0({
-    z: 6,
-  }).getCode(),
-);
-
-console.log(
-  new G0({
-    x: 7,
-    y: 8,
-    z: 9,
-  }).getCode(),
-);
+import G1test from './TEST/G1.test.js';
+G1test();
