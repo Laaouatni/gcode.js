a76b4086e846e5fa937241ea76ad8846481f39e9
laaouatni <laaouatni.anas@outlook.it>
2022-08-11 21:55:38 +0200

    make code not repetitve anymore for x, y z

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-class GcodeAPI {
-}
+class GcodeAPI {}
 
 class Gcommands extends GcodeAPI {
-  constructor() {
+  constructor(_xyzObj) {
     super();
-    this.lineOfCode = "⚠️ No line of code - please use getLine() method first";
+    this.lineOfCode = "⚠️ No line of code - please use getCode() method first";
+
+    this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
+    this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
+    this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
   }
 
   getCode() {
@@ -22,35 +25,22 @@ class Gcommands extends GcodeAPI {
 
 class G0 extends Gcommands {
   constructor(_xyzObj) {
-    super();
-    // these line are repetitive
-    this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
-    this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
-    this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
-
+    super(_xyzObj);
     this.prefix = `G0`;
   }
 }
 
 class G1 extends Gcommands {
   constructor(_xyzObj) {
-    super();
-    // these line are repetitive
-    this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
-    this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
-    this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
+    super(_xyzObj);
 
     this.prefix = `G1`;
   }
 }
 
-class G2 extends Gcommands { 
+class G2 extends Gcommands {
   constructor(_xyzObj) {
-    super();
-    // these line are repetitive
-    this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
-    this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
-    this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
+    super(_xyzObj);
 
     this.prefix = `G2`;
   }
@@ -58,14 +48,9 @@ class G2 extends Gcommands {
 
 class G3 extends Gcommands {
   constructor(_xyzObj) {
-    super();
-    // these line are repetitive
-    this.x = _xyzObj.x ?? GcodeAPI.previusX ?? 0;
-    this.y = _xyzObj.y ?? GcodeAPI.previusY ?? 0;
-    this.z = _xyzObj.z ?? GcodeAPI.previusZ ?? 0;
+    super(_xyzObj);
 
     this.prefix = `G3`;
   }
 }
 
-console.log(new G3({ x: 1, y: 2, z: 3 }).getLine());
