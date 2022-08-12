7f706637a7fe5d5ba9e08ccc82661a74582a46b8
laaouatni <laaouatni.anas@outlook.it>
2022-08-12 17:03:59 +0200

    added that console test can be desactivate so we can add more performance to the app

--- APP/TEST/test.js
+++ APP/TEST/test.js
@@ -1,22 +1,26 @@
-// GcodeAPI
-import GcodeApiTest from './GcodeAPI_main/GcodeAPI.test.js';
-GcodeApiTest();
-
-// G0
-import G0test from './Gcommands/G/G0.test.js';
-G0test();
-
-// G1
-import G1test from './Gcommands/G/G1.test.js';
-G1test();
-
-// Position Specificy
-import PositionSpecificyTest from './Specificy/PositionSpecificy.test.js';
-PositionSpecificyTest();
-
-// MoveTo 
-import MoveToTEST from './Gcommands/MoveTo/MoveTo.js';
-MoveToTEST();
-
+import GcodeApiTest from "./GcodeAPI_main/GcodeAPI.test.js";
+import G0test from "./Gcommands/G/G0.test.js";
+import G1test from "./Gcommands/G/G1.test.js";
+import PositionSpecificyTest from "./Specificy/PositionSpecificy.test.js";
+import MoveToTEST from "./Gcommands/MoveTo/MoveTo.js";
 import DirectionValuesTEST from "./DirectionValues/DirectionValues.test.js";
-DirectionValuesTEST();
+
+export default function consoleTEST() {
+  // GcodeAPI
+  GcodeApiTest();
+
+  // G0
+  G0test();
+
+  // G1
+  G1test();
+
+  // Position Specificy
+  PositionSpecificyTest();
+
+  // MoveTo
+  MoveToTEST();
+
+  // left, right, top, bottom, zIndex
+  DirectionValuesTEST();
+}
