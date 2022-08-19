7b49cb2e647cb5ba406afed7d85a437202dbe91c
laaouatni <laaouatni.anas@outlook.it>
2022-08-19 17:32:53 +0200

    changed demo

--- gcode/main.js
+++ gcode/main.js
@@ -1,24 +1,22 @@
 // import GcodeAPI from "./src/API/GcodeApi/GcodeAPI_main/GcodeAPI";
 import G0 from "./src/API/GcodeApi/Gcommands/G/G0";
-
 import Gcss from "./src/API/GcodeApi/Gsimulator/Gcss/Gcss.js";
 
-let a = new G0({ x: 10, y: 10 });
+let a = new G0({});
 
-a.moveTo({ left: 50});
-a.moveTo({ left: 100, top: 100 })
-a.moveTo({left: 200})
+for (let i = 0; i < 9; i++) {
+  if (i % 2 == 0) {
+    a.moveTo({ left: Math.random() * 100, top: Math.random() * 50 });
+  } else {
+    a.moveTo({ top: Math.random() * 100, left: Math.random() * 50 });
+  }
+}
 
-// 10, 10. 0
-
-// 60, 10
-
-// 160, -80
 
 let g = new Gcss({
   width: 500,
-  height: 400,
+  height: 500,
   parentHtmlContainer: document.querySelector("section"),
 });
 
-g.generate()
+g.generate();
