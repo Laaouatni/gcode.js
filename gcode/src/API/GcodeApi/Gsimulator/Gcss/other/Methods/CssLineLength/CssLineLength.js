e44700ba17a67a79107230726a3a5e680198829b
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2022-08-19 23:22:52 +0200

    changed CssLineLenght to be more readible in checking is same x, same y (but maybe more slower, but still more readible)

--- gcode/src/API/GcodeApi/Gsimulator/Gcss/other/Methods/CssLineLength/CssLineLength.js
+++ gcode/src/API/GcodeApi/Gsimulator/Gcss/other/Methods/CssLineLength/CssLineLength.js
@@ -3,21 +3,49 @@ export default class CssLineLength {
     this.obj = _this;
   }
 
-  calcolate(_this) {
+  calcolate() {
+    this.setObjSameOrNot();
+
+    if (
+      this.obj.currentObj.y !== this.obj.previusObj.y &&
+      this.obj.currentObj.x === this.obj.previusObj.x
+    ) {
+      return this.obj.PosDifference.y;
+    } else if (
+      this.obj.currentObj.x !== this.obj.previusObj.x &&
+      this.obj.currentObj.y === this.obj.previusObj.y
+    ) {
+      return this.obj.PosDifference.x;
+    } else if (
+      this.obj.currentObj.y !== this.obj.previusObj.y &&
+      this.obj.currentObj.x !== this.obj.previusObj.x
+    ) {
+      // pitagora's theorem
+      return Math.sqrt(
+        Math.pow(this.obj.PosDifference.x, 2) +
+          Math.pow(this.obj.PosDifference.y, 2),
+      );
+    } else {
+      // 0 means they aren't moving
+      return 0;
+    }
+  }
+
+  setObjSameOrNot() {
     if (
       this.obj.currentObj.y !== this.obj.previusObj.y &&
       this.obj.currentObj.x === this.obj.previusObj.x
     ) {
       this.obj.sameX = true;
       this.obj.sameY = false;
-      return this.obj.PosDifference.y;
+      this.obj.notSameXY = false;
     } else if (
       this.obj.currentObj.x !== this.obj.previusObj.x &&
       this.obj.currentObj.y === this.obj.previusObj.y
     ) {
       this.obj.sameY = true;
       this.obj.sameX = false;
-      return this.obj.PosDifference.x;
+      this.obj.notSameXY = false;
     } else if (
       this.obj.currentObj.y !== this.obj.previusObj.y &&
       this.obj.currentObj.x !== this.obj.previusObj.x
@@ -25,17 +53,10 @@ export default class CssLineLength {
       this.obj.sameX = false;
       this.obj.sameY = false;
       this.obj.notSameXY = true;
-      // pitagora's theorem
-      return Math.sqrt(
-        Math.pow(this.obj.PosDifference.x, 2) +
-          Math.pow(this.obj.PosDifference.y, 2),
-      );
     } else {
       this.obj.sameX = true;
       this.obj.sameY = true;
       this.obj.notSameXY = false;
-      // 0 means they aren't moving
-      return 0;
     }
   }
 }
