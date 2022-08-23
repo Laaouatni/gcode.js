45fd484edc472206856a585c101894916e424025
Laaouatni Anas <87947051+Laaouatni@users.noreply.github.com>
2022-08-23 22:58:01 +0200

    upadate example to show (random in all direction, start in the middle)

--- gcode/main.js
+++ gcode/main.js
@@ -4,48 +4,16 @@ import GcodeAPI from "./src/API/GcodeApi/GcodeAPI_main/GcodeAPI";
 import G0 from "./src/API/GcodeApi/Gcommands/G/G0";
 import Gcss from "./src/API/GcodeApi/Gsimulator/Gcss/Gcss.js";
 
-let a = new G0({});
+let a = new G0({x: 250, y: 250});
 
-for (let i = 0; i < 3; i++) {
-  a.moveTo({ left: 10, top: 30 });
-}
+for (let i = 0; i < 20; i++) {
+  let x = ["left", "right"][Math.round(Math.random())];
+  let y = ["top", "bottom"][Math.round(Math.random())];
 
-for (let i = 0; i < 3; i++) {
-  a.moveTo({ left: 100 });
-}
-
-for (let i = 0; i < 3; i++) {
   a.moveTo({
-    bottom: 50,
-  });
-}
-
-for (let i = 0; i < 3; i++) {
-  a.moveTo({
-    right: 100,
-  });
-}
-
-for (let i = 0; i < 3; i++) {
-  a.moveTo({
-    bottom: 50,
-    right: 50,
-  });
-}
-
-for (let i = 0; i < 3; i++) {
-  a.moveTo({
-    top: 50,
-    right: 20,
-  });
-}
-
-for (let i = 0; i < 4; i++) {
-  a.moveTo({ left: 50 });
-}
-
-for (let i = 0; i < 3; i++) {
-  a.moveTo({ bottom: 30, left: 50 });
+    [x]: Math.random() * 50,
+    [y]: Math.random() * 50
+  })
 }
 
 let g = new Gcss({
@@ -58,20 +26,3 @@ g.generate();
 
 const t1 = performance.now();
 console.log(`Call to doSomething took ${t1 - t0} milliseconds.`);
-// console.table(GcodeAPI.array);
-
-// let btn = document.querySelector("#btn")
-
-// btn.addEventListener("click", btnGenerate, { once: true });
-
-// function btnGenerate() {
-//   let g = new Gcss({
-//     width: 500,
-//     height: 500,
-//     parentHtmlContainer: document.querySelector("section"),
-//   });
-
-//   g.generate();
-
-//   btn.disabled = true;
-// }
