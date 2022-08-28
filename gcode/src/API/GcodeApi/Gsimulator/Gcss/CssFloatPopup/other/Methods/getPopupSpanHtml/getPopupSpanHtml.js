bd987b583639fbf1cfb7585fbe542e9c73d0d450
laaouatni <laaouatni.anas@outlook.it>
2022-08-28 22:19:19 +0200

    poppins font now isn't called anymore in css (typo fix)

--- gcode/src/API/GcodeApi/Gsimulator/Gcss/CssFloatPopup/other/Methods/getPopupSpanHtml/getPopupSpanHtml.js
+++ gcode/src/API/GcodeApi/Gsimulator/Gcss/CssFloatPopup/other/Methods/getPopupSpanHtml/getPopupSpanHtml.js
@@ -1,6 +1,6 @@
 export default function getPopupSpanHtml(_this) {
   return `
-  <div style="display: flex; align-items: center; gap: 1rem; width: min-content; font-family: poppins, system-ui;">
+  <div style="display: flex; align-items: center; gap: 1rem; width: min-content; font-family:   system-ui;">
     <div>
       <div style="display: flex; align-items: center; font-size: 1rem; gap: 0.5rem;">
         <div style="font-size: 1rem; font-weight: bold;">X</div>
@@ -27,7 +27,9 @@ export default function getPopupSpanHtml(_this) {
       <div style="display: flex; align-items: center; font-size: 1rem; gap: 0.5rem;">
         <div style="font-size: 1rem; font-weight: bold;">LENGTH</div>
         <div style="display: flex; align-items: baseline;">
-          <div>${JSON.parse(_this.button.dataset.line).length.split(".")[0]}</div>
+          <div>${
+            JSON.parse(_this.button.dataset.line).length.split(".")[0]
+          }</div>
           <div style="font-size: 0.7rem;">.${
             JSON.parse(_this.button.dataset.line).length.split(".")[1]
           }</div>
@@ -37,7 +39,9 @@ export default function getPopupSpanHtml(_this) {
       <div style="display: flex; align-items: center; font-size: 1rem; gap: 0.5rem;">
         <div style="font-size: 1rem; font-weight: bold;">ANGLE&nbsp;&nbsp;</div>
         <div style="display: flex; align-items: baseline;">
-          <div>${JSON.parse(_this.button.dataset.line).angle.split(".")[0]}</div>
+          <div>${
+            JSON.parse(_this.button.dataset.line).angle.split(".")[0]
+          }</div>
           <div style="font-size: 0.7rem;">.${
             JSON.parse(_this.button.dataset.line).angle.split(".")[1]
           }</div>
